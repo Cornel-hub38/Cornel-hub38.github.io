03c066f50b46da1915d280dc7b742bfe41ab627c
hp <csaearle38@gmail.com>
2025-02-05 10:19:44 +0000

    demo of forEach and arrow functions

---
+++ looping.js
@@ -0,0 +1,7 @@
+// looping in javascript using, for, for in, forEach
+// using forEach
+
+demoArray = ['alpha male', 10, "betamax", 35, "delta", 24.0]
+// use of forEach loop
+demoArray.forEach((element) => console.log(`${element}`));
+// output is ... alpha male \n  10 \n betamax \n 35 \n delta, 24
