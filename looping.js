1af9d305cbd7f49016cd63654026a63f4c596d77
hp <csaearle38@gmail.com>
2025-02-06 08:39:28 +0000

    main.html, forms and other input types - shayhowe.com:  email, paaaword, name, text date, week,

--- looping.js
+++ looping.js
@@ -5,3 +5,6 @@ demoArray = ['alpha male', 10, "betamax", 35, "delta", 24.0]
 // use of forEach loop
 demoArray.forEach((element) => console.log(`${element}`));
 // output is ... alpha male \n  10 \n betamax \n 35 \n delta, 24
+console.log("##############  Anonymous/ lambda functions ")
+kidding = "wow wow wow"
+console.log(lambda makeupperCase:   kidding.toUpperCase())
