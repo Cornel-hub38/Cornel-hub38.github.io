bd0f199f0f5c75e4af055432070fa427ce2145ff
hp <csaearle38@gmail.com>
2025-02-11 09:25:13 +0000

    javascript output testing

--- looping.js
+++ looping.js
@@ -7,4 +7,6 @@ demoArray.forEach((element) => console.log(`${element}`));
 // output is ... alpha male \n  10 \n betamax \n 35 \n delta, 24
 console.log("##############  Anonymous/ lambda functions ")
 kidding = "wow wow wow"
-console.log(lambda makeupperCase:   kidding.toUpperCase())
+// console.log(lambda makeupperCase:   kidding.toUpperCase())
+
+document.getElementById("bbc-welcome-2").innerHTML = "kidding my friend, just testing ya"
